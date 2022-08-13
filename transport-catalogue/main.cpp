f7078c67909c732062371c17fee0abab837be001
alexander @ubuntu-H310CH5-M2 <darkriderification@yandex.ru>
2022-08-13 14:04:29 +0500

    Программа работает на локальной машине,  но тесты в трнажере не проходят.

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -11,24 +11,23 @@
 #include <string_view>
 using namespace std::literals;
 
-
 void PrintUsage(std::ostream& stream = std::cerr) {
     stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
 }
+
 int main(int argc, char* argv[]) {
-    // if (argc != 2) {
-    //     PrintUsage();
-    //     return 1;
-    // }
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
 
-    // const std::string_view mode(argv[1]);
+    const std::string_view mode(argv[1]);
 
-    // if (mode == "make_base"sv) {
-        {
+    if (mode == "make_base"sv) {
+
         // make base here
-        // json::Document doc = json::Load(std::cin);
-        std::ifstream input_base_requests("s14_1_opentest_3_make_base.json");
-        json::Document doc = json::Load(input_base_requests);
+        json::Document doc = json::Load(std::cin);
+
         json_reader::JsonReader reader(doc);
 
         catalogue::TransportCatalogue cat;
@@ -49,15 +48,10 @@ int main(int argc, char* argv[]) {
             reader.ReadSerializeSettings(doc)
         );
         serializer_2000.Save();
-        }
+    } else if (mode == "process_requests"sv) {
 
-
-    // } else if (mode == "process_requests"sv) {
-        {
         // process requests here
-        // json::Document doc = json::Load(std::cin);
-        std::ifstream input_base_requests("s14_1_opentest_3_process_requests.json");
-        json::Document doc = json::Load(input_base_requests);
+        json::Document doc = json::Load(std::cin);
         json_reader::JsonReader reader(doc);
 
         Serialize::Deserializer deserializer(reader.ReadSerializeSettings(doc));
@@ -74,55 +68,10 @@ int main(int argc, char* argv[]) {
         RequestHandler handler(cat, renderer, router, transport_router);
 
         json::Document result = reader.ProcessStatRequests(handler);
-        std::ofstream output_result("s14_1_opentest_3_answer_my.json");
-        json::Print(result, output_result);
-        //json::Print(result, std::cout);
-        }
 
-
-    // } else {
-    //     PrintUsage();
-    //     return 1;
-    // }
+        json::Print(result, std::cout);
+    } else {
+        PrintUsage();
+        return 1;
+    }
 }
-// int main() {
-//     {
-      
-      
-//         json::Document doc = json::Load(std::cin);
-
-//         catalogue::TransportCatalogue cat;
-//         json_reader::JsonReader reader(doc);
-
-//         catalogue::TransportRouter transport_router(
-//             reader.ReadRoutingSettings(doc), 
-//             cat);
-
-//         // заполнение справочника и роутера
-//         reader.Fill(cat, transport_router);
-
-//         graph::Router<BusRouteWeight> router(transport_router.GetRouteGraph<BusRouteWeight>());
-
-//         renderer::MapRenderer renderer(reader.GetRenderSettings(), cat.GetBusesSorted());
-
-//         // ---- serialization moment! ++++
-//         Serialize::Serializer serializer_2000(
-//             cat,
-//             transport_router.GetRoutingSettings(),
-//             renderer.GetRenderSettings(),
-//             reader.ReadSerializeSettings(doc)
-//         );
-//         serializer_2000.SaveTo("debug_serialize.bin");
-//         Serialize::Deserializer deserializer("debug_serialize.bin");
-//         catalogue::TransportCatalogue second_catalogue(std::move(deserializer.GetTransportCatalogue()));
-//         catalogue::RoutingSettings routing_settings = deserializer.GetRoutingSettings();
-//         renderer::RenderSettings render_settings = deserializer.GetRenderSettings();
-
-//         RequestHandler handler(cat, renderer, router, transport_router);
-//         // ------------------------------------------ stage 2
-//         json::Document result = reader.ProcessStatRequests(handler);
-//         json::Print(result, std::cout);
-     
-//     }
-//     return 0;
-// }
